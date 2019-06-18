6ff4646add726ea743d284cb747f4def5b4cb6b0
EdwinSuubi <edwinsuubi@gmail.com>
2019-06-19 02:32:49 +0300

    [Bwowe]Creates function for AT commands testing

--- ESP8266.ino
+++ ESP8266.ino
@@ -11,3 +11,21 @@ int countTrueCommand;
 int countTimeCommand;
 boolean found = false;
 boolean wifiConnected = false;
+
+SoftwareSerial esp8266(espRX, espTX);
+
+void espSetup() {
+  esp8266.begin(115200);
+  sendATCommand("AT", 5, "OK"); // ping esp8266
+  sendATCommand("AT+CWMODE=3", 5, "OK"); // Set WiFi mode of operation -> 3 (AP + STA mode)
+}
+
+void testATcommands() { // MUST RUN ESP SETUP
+  if (esp8266.available()) {
+    Serial.write(esp8266.read());
+  }
+  if (Serial.available()) {
+    esp8266.write(Serial.read());
+  }
+}
+
