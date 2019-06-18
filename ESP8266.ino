a584b19681a3fe668697b080109b1b5979eae8b2
EdwinSuubi <edwinsuubi@gmail.com>
2019-06-19 02:29:23 +0300

    [Bwowe]Creates an instance and sets Wifi mode of operation

--- ESP8266.ino
+++ ESP8266.ino
@@ -1,1 +1,13 @@
+#define espRX 3
+#define espTX 2
 
+String netSSID = "Alvin";
+String psk = "123456789";
+String API = "DTH1GUWRS2KSWDQ1";
+String HOST = "api.thingspeak.com";
+String PORT = "80";
+String field = "field1";
+int countTrueCommand;
+int countTimeCommand;
+boolean found = false;
+boolean wifiConnected = false;
