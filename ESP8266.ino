dab496fe6bf800852afb793b111da546c691a1de
EdwinSuubi <edwinsuubi@gmail.com>
2019-06-19 03:42:04 +0300

    [Bwowe]Removes credentials from public access

--- ESP8266.ino
+++ ESP8266.ino
@@ -1,9 +1,7 @@
 #define espRX 3
 #define espTX 2
 
-String netSSID = "Alvin";
-String psk = "123456789";
-String API = "DTH1GUWRS2KSWDQ1";
+
 String HOST = "api.thingspeak.com";
 String PORT = "80";
 String field = "field1";
